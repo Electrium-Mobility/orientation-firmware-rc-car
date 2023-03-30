cc133a87d6597653af9f93ae5cde85ca32198c5e
Sherwin Chiu <sherwin.chiu89@gmail.com>
2023-03-30 16:24:56 -0400

    Add Websocket and Tutorial
    Websocket will allow connection between phone and ESP to be allowed.

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # orientation-firmware-rc-car
+This is the Firmware used for the RC Car
+
+## Setup
+You will need:
+#### Software
+- Arduino IDE, which you can install on https://www.arduino.cc/en/software 
+#### Hardware
+- D1 Mini Pro 
+- USB-C/Micro-USB Cable
+- A Phone to connect to Websocket
+
+## Introduction
+The ESP8266 creates a Joystick website server, that allows a user to send Analog values to the ESP8266.
+
+To do so, open the Arduino IDE and follow these steps:
+1. Go to File > Preferences > Additional Board Managers URLs, and copy and paste http://arduino.esp8266.com/stable/package_esp8266com_index.json
+2. Go to Tools > Board > Board Manager and search for esp8266 by ESP8266 Community. Install that package!
+3. Open WiFiAccessPoint.ino and select LOLIN(WEMOS) D1 mini Pro as your Microcontroller. Plug in your Microcontoller to the computer, and run the code. 
+4. From there, connect to the website and watch you move the analog stick!
+5. For hardware instructions follow: [some link here]
--- WiFiAccessPoint/WiFiAccessPoint.ino
+++ WiFiAccessPoint/WiFiAccessPoint.ino
@@ -1,6 +1,7 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
+#include <WebSocketsServer.h>
 #include "website.h"
 
 // define the SSID and password for the wifi access point
@@ -10,12 +11,18 @@ const char *password = "sherwin123";
 // create an instance of the ESP8266WebServer and initialize it to listen on port 80
 // 80 is the port used for HTTP requests
 ESP8266WebServer server(80);
+WebSocketsServer webSocket = WebSocketsServer(81); // Use port 81 for the WebSocket
 
 // handle HTTP requests to the root path ("/") of the web server
 // http://192.168.4.1 is connected to this access point
 void handleRoot() {
   Serial.println("Connected!");
   server.send(200, "text/html", site);
+}
+// handle websocket for real-time events
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
+  // Handle the event here
+  
 }
 
 // called once when the ESP8266 is powered on or reset
@@ -38,8 +45,11 @@ void setup() {
   // start web server
   server.begin();
   Serial.println("HTTP server started");
+  webSocket.begin();
+  webSocket.onEvent(webSocketEvent);
 }
 
 void loop() {
   server.handleClient();
+  webSocket.loop();
 }
