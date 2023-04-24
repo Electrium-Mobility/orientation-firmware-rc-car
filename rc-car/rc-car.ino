662c0f94a10b9b59cd07a821cc77c03047a0640d
sophie-wjyang <sophie.wjyang@gmail.com>
2023-04-24 18:10:20 -0400

    finalized firmware instructions solution

--- rc-car/rc-car.ino
+++ rc-car/rc-car.ino
@@ -1,3 +1,10 @@
+// HOW THE FUNCTIONS ARE CONNECTED
+// The setup() function is called once at the start of the program, and it sets up the wifi access point, server, and websocket
+// The loop() function is then called repeatedly to handle incoming HTTP requests and WebSocket events
+// When an HTTP request is received, the handleRoot() function is called to send the contents of website.h to the client
+// When a WebSocket event is received, the webSocketEvent() function is called to handle the event
+// The webSocketEvent() function deserializes a JSON message received from the client, extracts the values for the motor and servo control, and calls the run_motor() and steer() functions
+
 #include <ArduinoJson.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
@@ -16,6 +23,7 @@ WebSocketsServer webSocket = WebSocketsServer(81); // Use port 81 for the WebSoc
 Servo steering_servo;
 
 #include "functions.h"
+
 
 // **************************** HANDLE REQUESTS TO THE ROOT PATH OF THE WEB SERVER **************************** //
 // When a client connects to the server, the server sends the site content (in "website.h") to the client
@@ -105,12 +113,3 @@ void loop() {
   server.handleClient();
   webSocket.loop();
 }
-
-
-// HOW THE FUNCTIONS ARE CONNECTED
-// The setup() function is called once at the start of the program, and it sets up the wifi access point, server, and websocket
-// The loop() function is then called repeatedly to handle incoming HTTP requests and WebSocket events
-// When an HTTP request is received, the handleRoot() function is called to send the contents of website.h to the client
-// When a WebSocket event is received, the webSocketEvent() function is called to handle the event
-// The webSocketEvent() function deserializes a JSON message received from the client, extracts the values for the motor and servo control, and calls the run_motor() and steer() functions
-
